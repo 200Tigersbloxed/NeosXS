c0452ae84eb80d2d46f34df02c77da18df1ae89c
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2021-04-04 00:00:17 -0400

    Added AssemblyInfo

--- NeosXS_Headless/Properties/AssemblyInfo.cs
+++ NeosXS_Headless/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NeosXS_Headless")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Headless Server for the NeosXSAPI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NeosXS_Headless")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 200Tigersbloxed 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]

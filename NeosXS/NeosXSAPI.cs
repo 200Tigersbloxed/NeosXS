9d7f9cd690c03568baebf4480fd6e1b6d6cde62a
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2021-05-09 21:36:01 -0400

    Start of work for Plugin re-write

--- NeosXS/NeosXSAPI.cs
+++ NeosXS/NeosXSAPI.cs
@@ -1,4 +1,4 @@
-﻿//#define NEOS_PLUGIN
+﻿#define NEOS_PLUGIN
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 #if NEOS_PLUGIN
 using FrooxEngine;
-using BaseX;
 using FrooxEngine.UIX;
 #endif
 
@@ -14,7 +13,7 @@ namespace NeosXS
 {
 #if NEOS_PLUGIN
     [Category("NeosXS")]
-    class NeosXSAPI : Component, ICustomInspector
+    class NeosXSAPI : Component
     {
         public readonly Sync<bool> EnableWebsocket;
         public readonly Sync<string> Host;
@@ -76,6 +75,134 @@ namespace NeosXS
             WebsocketHelper.StopSocket();
             base.OnDestroy();
         }
+    }
+
+    public class NeosXSAPIUserListener: SimpleUserSpawn
+    {
+        public Dictionary<World, CacheUserFields> CurrentUsers;
+        public User LocalUser = null;
+        public World w1;
+        public World w2;
+
+        public override void OnUserJoined(User user)
+        {
+            GetAllUsers(CurrentUsers);
+            if (WebsocketHelper.IsSocketServerListening)
+            {
+                NotificationSender.OnUserJoined(user.UserName, user.Platform.ToString(), WebsocketHelper.XSOPort);
+            }
+            if (user.IsLocalUser)
+            {
+                LocalUser = user;
+            }
+            base.OnUserJoined(user);
+        }
+
+        public override void OnUserLeft(User user)
+        {
+            GetAllUsers(CurrentUsers);
+            if (WebsocketHelper.IsSocketServerListening)
+            {
+                NotificationSender.OnUserLeft(user.UserName, WebsocketHelper.XSOPort);
+            }
+            base.OnUserLeft(user);
+        }
+
+        public void GetAllUsers(Dictionary<World, CacheUserFields> DictionaryToApply)
+        {
+            if(LocalUser != null)
+            {
+                World currentWorld = LocalUser.World;
+                List<User> worldusers = null;
+                currentWorld.GetUsers(worldusers);
+                if(DictionaryToApply.ContainsKey(currentWorld))
+                {
+                    foreach(KeyValuePair<User, bool> entry in DictionaryToApply[currentWorld].WorldFocusCache)
+                    {
+                        if (entry.Key.World != currentWorld)
+                        {
+                            DictionaryToApply[currentWorld].WorldFocusCache.Remove(entry.Key);
+                        }
+                    }
+
+                    foreach (KeyValuePair<User, bool> entry in DictionaryToApply[currentWorld].HeadsetFocusCache)
+                    {
+                        if (entry.Key.World != currentWorld)
+                        {
+                            DictionaryToApply[currentWorld].HeadsetFocusCache.Remove(entry.Key);
+                        }
+                    }
+                }
+                else
+                {
+                    CacheUserFields cuf = new CacheUserFields();
+                    foreach (User user in worldusers)
+                    {
+                        cuf.WorldFocusCache.Add(user, user.IsPresentInWorld);
+                        cuf.HeadsetFocusCache.Add(user, user.IsPresentInHeadset);
+                    }
+                    DictionaryToApply.Add(currentWorld, cuf);
+                }
+            }
+            else { LogHelper.Error("LocalUser is null! Cannot get users!"); }
+        }
+
+        protected override void OnBehaviorUpdate()
+        {
+            // world check
+            if(LocalUser != null)
+            {
+                if(w1 == null)
+                {
+                    w1 = LocalUser.World;
+                }
+                else
+                {
+                    w2 = w1;
+                    w1 = LocalUser.World;
+                }
+            }
+            else { LogHelper.Error("LocalUser is null! Cannot get users!"); }
+
+            if(w1 != w2)
+            {
+                if (!w2.LocalUser.IsPresent)
+                {
+                    CurrentUsers.Remove(w2);
+                }
+            }
+            // user check
+            if(w1 != null)
+            {
+                foreach (KeyValuePair<User, bool> entry in CurrentUsers[w1].WorldFocusCache)
+                {
+                    User user = entry.Key;
+                    if(entry.Value != user.IsPresentInWorld)
+                    {
+                        NotificationSender.UserPresentInHeadsetChanged(user.UserName, user.IsPresentInWorld, WebsocketHelper.XSOPort);
+                        CurrentUsers[w1].WorldFocusCache[user] = user.IsPresentInWorld;
+                    }
+                }
+
+                foreach(KeyValuePair<User, bool> entry in CurrentUsers[w1].HeadsetFocusCache)
+                {
+                    User user = entry.Key;
+                    if(entry.Value != user.IsPresentInWorld)
+                    {
+                        NotificationSender.UserPresentInWorldChanged(user.UserName, user.IsPresentInWorld, WebsocketHelper.XSOPort);
+                        CurrentUsers[w1].WorldFocusCache[user] = user.IsPresentInHeadset;
+                    }
+                }
+            }
+
+            base.OnBehaviorUpdate();
+        }
+    }
+
+    public class CacheUserFields
+    {
+        public Dictionary<User, bool> WorldFocusCache;
+        public Dictionary<User, bool> HeadsetFocusCache;
     }
 #else
     public class NeosXSAPI
